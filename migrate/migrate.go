26ff0e7602998f26387d94d48569bab31be0df23
Patarapong Limvipaveeanan <pat_opor@hotmail.com>
2023-12-19 15:22:30 +0700

    added auto migrate at migrate/migrate.go

---
+++ migrate/migrate.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/woonmapao/user-management/initializer"
+	"github.com/woonmapao/user-management/models"
+	_ "github.com/woonmapao/user-management/models"
+)
+
+func init() {
+	initializer.LoadEnvVariables()
+	initializer.DBInitializer()
+}
+
+func main() {
+	initializer.DB.AutoMigrate(&models.UserDetail{})
+}
