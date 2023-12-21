6630d79764405102b4cb27c853e42bd6e3fe7e8b
Patarapong Limvipaveeanan <pat_opor@hotmail.com>
2023-12-21 18:40:55 +0700

    Added struct tags

--- models/user_model.go
+++ models/user_model.go
@@ -6,8 +6,8 @@ import (
 
 type User struct {
 	gorm.Model
-	FirstName   string
-	LastName    string
-	Email       string
-	PhoneNumber string
+	FirstName   string `json:"firstName"`
+	LastName    string `json:"lastName"`
+	Email       string `json:"email"`
+	PhoneNumber string `json:"phoneNumber"`
 }
