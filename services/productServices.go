7146ba99291607f582f3c943e74c7701961f923e
Patarapong Limvipaveeanan <pat_opor@hotmail.com>
2023-12-22 14:06:48 +0700

    Moved SearchProducts() to /services

--- services/productServices.go
+++ services/productServices.go
@@ -1,6 +1,9 @@
 package services
 
 import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 	"github.com/woonmapao/user-management/initializer"
 	"github.com/woonmapao/user-management/models"
 )
@@ -17,4 +20,24 @@ func UpdateStock(productID, quantity int) error {
 	initializer.DB.Model(&product).Update("StockQuantity", newStock)
 
 	return nil
+}
+
+func SearchProducts(c *gin.Context) {
+	// Extract search criteria from the request parameters or query string
+	searchCriteria := c.Query("searchCriteria")
+
+	// Query the database for products that match the criteria
+	var searchResults []models.Product
+	err := initializer.DB.Where("name LIKE ?", "%"+searchCriteria+"%").Find(&searchResults).Error
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to fetch search results",
+		})
+		return
+	}
+
+	// Return a JSON response with the search results
+	c.JSON(http.StatusOK, gin.H{
+		"searchResults": searchResults,
+	})
 }
