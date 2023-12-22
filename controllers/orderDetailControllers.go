ba54b83bf7a5b69c77a44f72289f0cfd5f0d49b2
Patarapong Limvipaveeanan <pat_opor@hotmail.com>
2023-12-22 09:39:06 +0700

    Complete CreateOrderDetail()

--- controllers/orderDetailControllers.go
+++ controllers/orderDetailControllers.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -46,12 +47,77 @@ func GetOrderDetailByID(c *gin.Context) {
 }
 
 func CreateOrderDetail(c *gin.Context) {
-	// Handle the creation of a new order detail
-
 	// Extract order detail data from the request body
+	var orderDetailData struct {
+		OrderID   int     `json:"orderId" binding:"required"`
+		ProductID int     `json:"productId" binding:"required"`
+		Quantity  int     `json:"quantity" binding:"required,gte=1"`
+		Subtotal  float64 `json:"subtotal"`
+	}
+
+	err := c.ShouldBindJSON(&orderDetailData)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
 	// Validate the input data
+	err = validateOrderDetailData(orderDetailData)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
 	// Create a new order detail in the database
+	orderDetail := models.OrderDetail{
+		OrderID:   orderDetailData.OrderID,
+		ProductID: orderDetailData.ProductID,
+		Quantity:  orderDetailData.Quantity,
+		Subtotal:  orderDetailData.Subtotal,
+	}
+
+	initializer.DB.Create(&orderDetail)
+
 	// Return a JSON response with the newly created order detail
+	c.JSON(http.StatusCreated, gin.H{
+		"createdOrderDetail": orderDetail,
+	})
+}
+
+// Validate order detail data
+func validateOrderDetailData(data struct {
+	OrderID   int     `json:"orderId" binding:"required"`
+	ProductID int     `json:"productId" binding:"required"`
+	Quantity  int     `json:"quantity" binding:"required,gte=1"`
+	Subtotal  float64 `json:"subtotal"`
+}) error {
+
+	// Example: Check if the order and product exist in the database
+	if !orderExists(data.OrderID) {
+		return fmt.Errorf("order with ID %d does not exist", data.OrderID)
+	}
+
+	if !productExists(data.ProductID) {
+		return fmt.Errorf("product with ID %d does not exist", data.ProductID)
+	}
+
+	// Check if the product has sufficient stock
+	if !hasSufficientStock(data.ProductID, data.Quantity) {
+		return fmt.Errorf("insufficient stock for product with ID %d", data.ProductID)
+	}
+
+	return nil
+}
+
+// Check if an order with the given ID exists in the database
+func orderExists(orderID int) bool {
+	var order models.Order
+	result := initializer.DB.First(&order, orderID)
+	return result.RowsAffected > 0
 }
 
 func UpdateOrderDetail(c *gin.Context) {
